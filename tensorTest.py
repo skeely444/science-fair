95ca84b90238b0755b92f037a84ab53ba0fde806
skeely444 <115996710+skeely444@users.noreply.github.com>
2026-02-10 20:56:08 +0100

    10/02/2026
    Experimented with the different epoch times, currently 45 is the sweet spot since most results their sweetspot were around 45.

--- tensorTest.py
+++ tensorTest.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 print("TensorFlow version:", tf.__version__)
 
 xTrain = np.load("X_train.npy")
@@ -22,7 +23,35 @@ model.compile(optimizer = 'adam',
               loss = loss_fn,
               metrics=['accuracy'])
 
-model.fit(xTrain, yTrain, epochs=50, batch_size=32, validation_split=0.2)
+history = model.fit(xTrain, yTrain, epochs=45, batch_size=32, validation_split=0.2)
 test_loss, test_accuracy = model.evaluate(xTest, yTest, verbose=1)
 print(f"\n🎉 FINAL TEST ACCURACY: {test_accuracy * 100:.2f}%")
-model.save("SAI.keras")
+Yprediction = model.predict(xTest)
+labledYPred = np.argmax(Yprediction, axis=1)
+trueY_labels = yTest
+ct = tf.math.confusion_matrix(labels=trueY_labels, predictions=labledYPred)
+print(ct.numpy())
+
+plt.style.use("ggplot")
+plt.figure(figsize=(12, 5))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label="Training accuracy")
+plt.plot(history.history['val_accuracy'], label="validation accuracy")
+plt.title("Accuracy over epoches")
+plt.xlabel("Epoches")
+plt.ylabel("Accuracy")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label="Training loss")
+plt.plot(history.history['val_loss'], label="Validation loss")
+plt.title("Loss over epoches")
+plt.xlabel("Epoches")
+plt.ylabel("Loss")
+plt.legend()
+
+plt.tight_layout()
+plt.savefig("Training graphs.png")
+plt.show()
+model.save(f"SAI{test_accuracy}.keras")
